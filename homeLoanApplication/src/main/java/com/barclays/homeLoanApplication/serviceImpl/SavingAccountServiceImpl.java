a31076d2139afc074c6a20124327d75b9965b267
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2022-08-24 14:49:24 +0530

    Edits

--- homeLoanApplication/src/main/java/com/barclays/homeLoanApplication/serviceImpl/SavingAccountServiceImpl.java
+++ homeLoanApplication/src/main/java/com/barclays/homeLoanApplication/serviceImpl/SavingAccountServiceImpl.java
@@ -4,9 +4,11 @@ import com.barclays.homeLoanApplication.entity.SavingAccount;
 import com.barclays.homeLoanApplication.repository.SavingAccountRepository;
 import com.barclays.homeLoanApplication.service.SavingAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SavingAccountServiceImpl implements SavingAccountService {
     @Autowired
     private SavingAccountRepository savingAccountRepository;
