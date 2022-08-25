686a9faa784b887898f0878ba7596451045c2680
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2022-08-25 11:16:30 +0530

    EMI Calculation Changes

--- homeLoanApplication/src/main/java/com/barclays/homeLoanApplication/constants/LoanApplicationConstants.java
+++ homeLoanApplication/src/main/java/com/barclays/homeLoanApplication/constants/LoanApplicationConstants.java
@@ -3,5 +3,5 @@ package com.barclays.homeLoanApplication.constants;
 public interface LoanApplicationConstants {
     public static final long MINIMUM_LOAN_TENURE = 5;
     public static final long MAXIMUM_LOAN_TENURE = 20;
-    public static final double RATE_OF_INTEREST = 0.07F;
+    public static final double RATE_OF_INTEREST = 7.0F;
 }
